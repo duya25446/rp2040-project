d66e66f8f6ac665f6bda5a9683d66d0fef2a03ce
duya25446 <i145463912@yahoo.com>
2023-12-20 02:10:01 +0800

    config Wokwi for VS Code

--- main.c
+++ main.c
@@ -5,12 +5,14 @@ int main() {
 
 #else
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init_mask(0b11111111);
+    gpio_set_dir_masked(0b11111111,0b11111111);
     while (true) {
-        gpio_put(LED_PIN, 1);
+        //gpio_put(LED_PIN, 1);
+        gpio_put_all(0b11111111);
         sleep_ms(250);
-        gpio_put(LED_PIN, 0);
+        //gpio_put(LED_PIN, 0);
+        gpio_put_all(0);
         sleep_ms(250);
     }
 #endif
